92d8197840e252aee64f0d06804318a458c1253e
sc2bat <izar_dero@naver.com>
2023-10-10 18:31:43 +0900

    test dart to html

--- 003_connect_html/index.dart
+++ 003_connect_html/index.dart
@@ -3,4 +3,6 @@ import 'dart:html';
 void main() {
   final header = querySelector('#header');
   header?.text = "Hello, World!";
+
+  querySelector('#RipVanWinkle')!.text = 'Wake up, sleepy head!';
 }
