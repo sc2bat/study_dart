51edec647da48a5e534daf104b8d58a5547687fa
sc2bat <izar_dero@naver.com>
2023-10-19 18:04:55 +0900

    install flutter done

---
+++ 004_test_project/test_001/bin/test_001.dart
@@ -0,0 +1,5 @@
+import 'package:test_001/test_001.dart' as test_001;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_001.calculate()}!');
+}
---
+++ 004_test_project/test_001/test/test_001_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_001/test_001.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
