8ab4e2c544083f37b61ca6fcf9fe3d44e9fb2ff4
stw26 <it.stw26@contentlap.com>
2023-11-18 13:41:05 +0900

    start stateful widget flutter

---
+++ 100_study_flutter/toonapp/lib/stateful_widgets/main_01.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:toonapp/main.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+void main() {
+  runApp(const App());
+}
