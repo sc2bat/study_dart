f35d6560e99fc3abc2129edb2fec02cc1081f2f7
stw26 <it.stw26@contentlap.com>
2023-11-18 14:23:50 +0900

    add logger dependencies

--- 100_study_flutter/toonapp/lib/stateful_widgets/main_01.dart
+++ 100_study_flutter/toonapp/lib/stateful_widgets/main_01.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:toonapp/main.dart';
+import 'package:logger/logger.dart';
+// import 'package:toonapp/main.dart';
+
+var logger = Logger();
+
+void main() {
+  runApp(const App());
+}
 
 class App extends StatefulWidget {
   const App({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+  State<App> createState() => _AppState();
 }
 
-void main() {
-  runApp(const App());
+class _AppState extends State<App> {
+  int counter = 0;
+
+  List<int> numbers = [];
+
+  void onClicked() {
+    setState(() {
+      counter += 1;
+      numbers.add(numbers.length);
+    });
+    // print(numbers);
+    logger.d(numbers);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white70,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Click counter",
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                "$counter",
+                style: const TextStyle(fontSize: 30),
+              ),
+              for (var number in numbers) Text("$number"),
+              IconButton(
+                iconSize: 30,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
