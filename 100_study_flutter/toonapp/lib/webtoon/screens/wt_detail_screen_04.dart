123bd3de7d72fd87cc25a543542b8f48541a029c
stw26 <it.stw26@contentlap.com>
2023-11-21 09:53:35 +0900

    webtoon app done

--- 100_study_flutter/toonapp/lib/webtoon/screens/wt_detail_screen_04.dart
+++ 100_study_flutter/toonapp/lib/webtoon/screens/wt_detail_screen_04.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonapp/webtoon/service/wt_api_service_03.dart';
 import 'package:toonapp/webtoon/widget/wt_episode_widget_01.dart';
 import 'package:toonapp/webtoon/wt_models/wt_webtoon_detail_model.dart';
@@ -22,11 +23,50 @@ class _DetailScreen04State extends State<DetailScreen04> {
   late Future<WebtoonDetailModel01> webtoon;
   late Future<List<WebtoonEpisodeModel01>> webtoonEpisodes;
 
+  // get liked
+  late SharedPreferences prefs;
+  // check liked bool
+  bool isLiked = false;
+
+  Future initPreferences() async {
+    prefs = await SharedPreferences.getInstance();
+    final List<String>? likedToon = prefs.getStringList('likedToon');
+
+    if (likedToon != null) {
+      if (likedToon.contains(widget.id) == true) {
+        isLiked = true;
+        setState(() {});
+      }
+    } else {
+      prefs.setStringList("likedToon", []);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     webtoon = ApiService03.getDetailById(widget.id);
     webtoonEpisodes = ApiService03.getEpisodeById(widget.id);
+
+    // get liked init
+    initPreferences();
+  }
+
+  // likedToons : [1, 2, 3, 4];
+  onHeartTap() async {
+    final List<String>? likedToon = prefs.getStringList('likedToon');
+    if (likedToon != null) {
+      if (isLiked) {
+        likedToon.remove(widget.id);
+      } else {
+        likedToon.add(widget.id);
+      }
+      await prefs.setStringList("likedToon", likedToon);
+
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
   }
 
   @override
@@ -39,10 +79,12 @@ class _DetailScreen04State extends State<DetailScreen04> {
         elevation: 5,
         backgroundColor: Colors.white70,
         foregroundColor: Colors.blueAccent,
-        actions: const [
+        actions: [
           IconButton(
-            onPressed: null,
-            icon: Icon(Icons.favorite_border_outlined),
+            onPressed: onHeartTap,
+            icon: Icon(
+              isLiked ? Icons.favorite : Icons.favorite_border_outlined,
+            ),
           ),
         ],
         title: Text(
